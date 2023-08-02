27391846a5d3eadc8d12705e3873f53d12015820
ashwinjain99 <ajainy99@gmail.com>
2023-08-02 01:12:47 -0700

    Updating schema

--- EmployeeSQL/Table_Schema.sql
+++ EmployeeSQL/Table_Schema.sql
@@ -1,36 +1,42 @@
 CREATE TABLE IF NOT EXISTS departments(
 	dept_no VARCHAR(4) PRIMARY KEY,
-	dept_name VARCHAR  	
+	dept_name VARCHAR NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS dept_manager(
-	dept_no VARCHAR(4) PRIMARY KEY,
-	emp_no INT	
+	dept_no VARCHAR(4),
+	emp_no INTEGER,
+	PRIMARY KEY(dept_no, emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 )
 
 CREATE TABLE IF NOT EXISTS dept_emp(
-	emp_no INT,  
+	emp_no INTEGER,  
 	dept_no VARCHAR(4),
-	PRIMARY KEY(emp_no, dept_no)
+	PRIMARY KEY(emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 )
 
 CREATE TABLE IF NOT EXISTS employees(
-	emp_no INT,  PRIMARY KEY
-	emp_title_id VARCHAR(5) REFERENCES titles (title_id),
-	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR(1),
-	hire_date DATE
+	emp_no INTEGER PRIMARY KEY,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 )
 
 CREATE TABLE IF NOT EXISTS titles(
-	title_id VARCHAR(5), PRIMARY KEY
-	title VARCHAR 
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS salaries(
-	emp_no INT,
-	salary INT
-	PRIMARY KEY (emp_no, salary)
+	emp_no INTEGER PRIMARY KEY,
+	salary BIGINT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 )
