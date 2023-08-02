2a6fdb55567212c33f8dd4ac945587abb8031f69
ashwinjain99 <ajainy99@gmail.com>
2023-08-02 01:17:51 -0700

    Updating table schemas

--- EmployeeSQL/Table_Schema.sql
+++ EmployeeSQL/Table_Schema.sql
@@ -3,12 +3,15 @@ CREATE TABLE IF NOT EXISTS departments(
 	dept_name VARCHAR NOT NULL
 )
 
-CREATE TABLE IF NOT EXISTS dept_manager(
-	dept_no VARCHAR(4),
-	emp_no INTEGER,
-	PRIMARY KEY(dept_no, emp_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+CREATE TABLE IF NOT EXISTS employees(
+	emp_no INTEGER PRIMARY KEY,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 )
 
 CREATE TABLE IF NOT EXISTS dept_emp(
@@ -19,15 +22,12 @@ CREATE TABLE IF NOT EXISTS dept_emp(
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 )
 
-CREATE TABLE IF NOT EXISTS employees(
-	emp_no INTEGER PRIMARY KEY,
-	emp_title_id VARCHAR(5) NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR(1) NOT NULL,
-	hire_date DATE NOT NULL,
-	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+CREATE TABLE IF NOT EXISTS dept_manager(
+	dept_no VARCHAR(4),
+	emp_no INTEGER,
+	PRIMARY KEY(dept_no, emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 )
 
 CREATE TABLE IF NOT EXISTS titles(
